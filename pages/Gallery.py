59e5b4a7df1b0f5ca263b35e3c98a5779f590c64
Remco Coppens <remcocoppens@outlook.com>
2022-06-11 13:39:32 +0200

    Update Gallery.py

--- pages/Gallery.py
+++ pages/Gallery.py
@@ -19,7 +19,7 @@ st.markdown("# Gallery 📸")
 st.sidebar.markdown("# Gallery 📸")
 col1, col2, col3, col4 = st.columns(4)
 
-files = fs.ls('openday2022streamlit/Fotos_HTCopenday2022')[17:29]
+files = fs.ls('openday2022streamlit/Fotos_HTCopenday2022')[27:39]
 bytes = [read_image(file) for file in files if file.endswith('.jpeg')]
 images = [bytes_to_image(img) for img in bytes if len(img) > 0]
 
